0cdeb9779f2ea9ba44ee69633a204037d66da806
sudesh7447 <85831715+sudesh7447@users.noreply.github.com>
2022-05-14 11:12:26 +0530

    Revert "Firebase added"

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:op/landing.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
+import 'home.dart';
 
+void main() {
+  runApp( MyApp());
+}
 class MyApp extends StatelessWidget {
+
   const MyApp({Key? key}) : super(key: key);
 
   @override
